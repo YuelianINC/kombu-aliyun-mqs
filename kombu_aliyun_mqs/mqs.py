c385e256c9c020effde03f10bb73f323e4548973
ember <zghpku@gmail.com>
2014-12-07 15:42:57 +0800

    fix bugs

--- kombu_aliyun_mqs/mqs.py
+++ kombu_aliyun_mqs/mqs.py
@@ -50,6 +50,7 @@ class Channel(virtual.Channel):
         # exists with a different visibility_timeout, so this prepopulates
         # the queue_cache to protect us from recreating
         # queues that are known to already exist.
+        logger.debug("create channel")
         req = ListQueueRequest()
         resp = ListQueueResponse()
         self.mqs_client.list_queue(req, resp)
@@ -67,6 +68,7 @@ class Channel(virtual.Channel):
         self._queue_message_cache = collections.deque()
 
     def basic_consume(self, queue, no_ack, *args, **kwargs):
+        logger.debug("basic_consume:" + queue)
         if no_ack:
             self._noack_queues.add(queue)
         return super(Channel, self).basic_consume(
@@ -74,37 +76,63 @@ class Channel(virtual.Channel):
         )
 
     def basic_cancel(self, consumer_tag):
+        logger.debug("basic_cancel" + consumer_tag)
         if consumer_tag in self._consumers:
             queue = self._tag_to_queue[consumer_tag]
             self._noack_queues.discard(queue)
         return super(Channel, self).basic_cancel(consumer_tag)
 
     def drain_events(self, timeout=None):
-        """Return a single payload message from one of our queues.
-        :raises Empty: if no messages available.
-        """
-        # If we're not allowed to consume or have no consumers, raise Empty
-        if not self._consumers or not self.qos.can_consume():
-            raise Empty()
-        message_cache = self._queue_message_cache
-
-        # Check if there are any items in our buffer. If there are any, pop
-        # off that queue first.
         try:
-            return message_cache.popleft()
-        except IndexError:
-            pass
+            logger.debug("drain_events")
+            """Return a single payload message from one of our queues.
+            :raises Empty: if no messages available.
+            """
+            # If we're not allowed to consume or have no consumers, raise Empty
+            if not self._consumers or not self.qos.can_consume():
+                logger.debug("raise empty")
+                raise Empty()
+            message_cache = self._queue_message_cache
 
-        res, queue = self._poll(self.cycle, timeout=timeout)
-        message_cache.extend((r, queue) for r in res)
+            # Check if there are any items in our buffer. If there are any, pop
+            # off that queue first.
+            logger.debug("first")
+            try:
+                return message_cache.popleft()
+            except IndexError:
+                logger.debug("indexerror 1")
+                pass
+            except Exception,e:
+                logger.error(e)
 
-        # Now try to pop off the queue again.
-        try:
-            return message_cache.popleft()
-        except IndexError:
-            raise Empty()
+            logger.debug(self._active_queues)
+            res, queue = self._poll(self.cycle, timeout=timeout)
+            # logger.debug(res)
+            message_cache.extend((r, queue) for r in res)
+
+            # Now try to pop off the queue again.
+            logger.debug("second")
+            try:
+                m = message_cache.popleft()
+                logger.debug("second:" + repr(m))
+                return m
+            except IndexError:
+                logger.debug("indexerror 2")
+                raise Empty()
+            except Exception,e:
+                logger.error(e)
+        except Exception,e:
+            import traceback
+            logger.debug("54321")
+            print str(e)
+            traceback.print_exc()
+            # print traceback.format_exc()
+    #or
+            # print sys.exc_info()[0]
+            logger.debug("12345")
 
     def _reset_cycle(self):
+        logger.debug("reset_cycle")
         """Reset the consume cycle.
         :returns: a FairCycle object that points to our _get_bulk() method
           rather than the standard _get() method. This allows for multiple
@@ -121,28 +149,41 @@ class Channel(virtual.Channel):
 
     def _new_queue(self, queue, **kwargs):
         """Ensure a queue with given name exists in SQS."""
+        # logger.debug(type(queue))
+        if isinstance(queue,unicode):
+            queue = queue.encode('utf-8')
+        # queue = str(queue)
+        logger.debug("_new_queue:" + queue)
+        # logger.debug(type(queue))
+        queue = queue.replace('.','-')
+        queue = queue.replace('@','-')
         if not isinstance(queue, string_t):
             return queue
         # Translate to SQS name for consistency with initial
         # _queue_cache population.
         queue = self.queue_name_prefix + queue
         try:
+            logger.debug("_new_queue: normal")
             return self._queue_cache[queue]
         except KeyError:
+            logger.debug("_new_queue: keyerror123")
             the_queue = Queue(queue, self.mqs_client)
             queue_meta = QueueMeta()
-            queue_meta.set_visibilitytimeout(self.visibility_timeout)
-            the_queue.create(queue_meta)
+            queue_meta.set_visibilitytimeout(self.default_visibility_timeout)
+            qurl = the_queue.create(queue_meta)
+            logger.debug(qurl)
             self._queue_cache[queue] = the_queue
             return the_queue
 
     def _delete(self, queue, *args):
+        logger.debug("_delete:" + queue)
         """delete queue by name."""
         super(Channel, self)._delete(queue)
         self._queue_cache.pop(queue, None)
 
     def _put(self, queue, message, **kwargs):
         """Put message onto queue."""
+        logger.debug("_put" + queue + ' ' + dumps(message))
         q = self._new_queue(queue)
         m = Message()
         m.message_body = (dumps(message))
@@ -161,14 +202,14 @@ class Channel(virtual.Channel):
     #             self._loop1(queue)
 
     def _message_to_python(self, message, queue_name, queue):
-        payload = loads(message.get_body())
+        payload = loads(bytes_to_str(message.get_body()))
         if queue_name in self._noack_queues:
             queue.delete_message(message.receipt_handle)
         else:
             payload['properties']['delivery_info'].update({
                 'mqs_message': message, 'mqs_queue': queue,
             })
-        return payload
+        return [payload]
 
     def _messages_to_python(self, messages, queue):
         """Convert a list of SQS Message objects into Payloads.
@@ -184,6 +225,7 @@ class Channel(virtual.Channel):
 
 
     def _get(self, queue):
+        logger.debug('_get:' + queue)
         """Try to retrieve a single message off ``queue``."""
         q = self._new_queue(queue)
         message = q.receive_message()
@@ -199,6 +241,7 @@ class Channel(virtual.Channel):
         )
 
     def _on_messages_ready(self, queue, qname, messages):
+        logger.debug('on_message_ready')
         if messages:
             callbacks = self.connection._callbacks
             for raw_message in messages:
@@ -210,6 +253,7 @@ class Channel(virtual.Channel):
         """Retrieve and handle messages from SQS.
         Uses long polling and returns :class:`~amqp.promise`.
         """
+        logger.debug('get_from_mqs')
         mqs_client = mqs_client if mqs_client is not None else queue.mqs_client
         return queue.receive_message()
         # return connection.receive_message(
@@ -220,12 +264,14 @@ class Channel(virtual.Channel):
 
     def _restore(self, message,
                  unwanted_delivery_info=('mqs_message', 'mqs_queue')):
+        logger.debug('restore')
         for unwanted_key in unwanted_delivery_info:
             # Remove objects that aren't JSON serializable (Issue #1108).
             message.delivery_info.pop(unwanted_key, None)
         return super(Channel, self)._restore(message)
 
     def basic_ack(self, delivery_tag):
+        logger.debug('basic_ack')
         delivery_info = self.qos.get(delivery_tag).delivery_info
         try:
             queue = delivery_info['mqs_queue']
@@ -237,9 +283,14 @@ class Channel(virtual.Channel):
 
     def _size(self, queue):
         """Return the number of messages in a queue."""
-        return self._new_queue(queue).get_attributes().active_messages
+        q = self._new_queue(queue)
+        attr = q.get_attributes()
+        res = attr.active_messages
+        logger.debug('_size:'+ str(res))
+        return res
 
     def _purge(self, queue):
+        logger.debug('purge')
         """Delete all current messages in a queue."""
         q = self._new_queue(queue)
         # SQS is slow at registering messages, so run for a few
